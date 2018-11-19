026423f7e9edb9f234d167b72b0e86a0b1185562
Adin Kwok <adin.kwok@carbonrom.org>
2018-11-19 11:39:12 -0800

    Add currency item layout
    Time to go to school and work on this later.

--- app/src/main/java/com/adinkwok/cryptocharts/CryptoChartsActivity.java
+++ app/src/main/java/com/adinkwok/cryptocharts/CryptoChartsActivity.java
@@ -1,7 +1,14 @@
 package com.adinkwok.cryptocharts;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Objects;
 
 public class CryptoChartsActivity extends AppCompatActivity {
 
@@ -9,5 +16,26 @@ public class CryptoChartsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crypto_charts);
+
+        LinearLayout parent = findViewById(R.id.currency_layout);
+        for (int i = 0; i < 12; i++) {
+            parent.addView(makeCurrencyItem());
+        }
+
+        Objects.requireNonNull(getSupportActionBar())
+                .setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.abs_layout);
+    }
+
+    @SuppressLint("InflateParams")
+    View makeCurrencyItem() {
+        LinearLayout currencyItem = (LinearLayout) getLayoutInflater()
+                .inflate(R.layout.currency_item_layout, null);
+        TextView currencyName = currencyItem.findViewById(R.id.currency_name);
+        TextView currencyPrice = currencyItem.findViewById(R.id.currency_price_in_cad);
+        String formattedPrice = getString(R.string.currency_price);
+        currencyName.setText(R.string.currency_test_name);
+        currencyPrice.setText(String.format(formattedPrice, getString(R.string.currency_test_price)));
+        return currencyItem;
     }
 }
